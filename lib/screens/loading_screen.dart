fa98ebd377c38ec4d1c99fa6b3bcfad2a5fbc394
DESKTOP-SKMPL8O\Legend Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-11-18 14:38:39 +0330

    Get weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -13,6 +14,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -20,6 +22,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     debugPrint('location latitude ${location.latitude}');
     debugPrint('location longitude ${location.longitude}');
+  }
+
+  void getData ()async{
+    Response response= await get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=500c9d7b7987ee49fecbeb4f04feae0e'));
+    if (response.statusCode == 200){
+      String data = response.body;
+      debugPrint('current weather: $data');
+    }else{
+      debugPrint(response.statusCode.toString());
+    }
   }
 
   @override
